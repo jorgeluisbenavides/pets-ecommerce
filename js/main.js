2ebe6cbea3ed7e62452b5590c7562c917a5cb64f
Jorge Benavides <jorge.benavides@facturatech.co>
2025-08-05 22:09:48 -0600

    Local storage

--- js/main.js
+++ js/main.js
@@ -1,33 +1,89 @@
+const expresionEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+const expresionPassword = /^[{a-z}+{A-Z}]+$/;
+
 const formLogin = document.getElementById("formLogin");
-formLogin.addEventListener("click", function (event) {
-  signIn(event);
-});
+if (formLogin !== null) {
+  formLogin.addEventListener("click", function (event) {
+    signIn(event);
+  });
+}
+
+const isExistMenu = document.getElementById("menu");
+if (isExistMenu) {
+  showUserName();
+}
+
+function showUserName() {
+  const sesion = localStorage.getItem("sesion");
+  if (sesion) {
+    const li = document.createElement("li");
+    const a = document.createElement("a");
+
+    const icono =
+      "<span class='material-symbols-outlined' style='font-size: 15px;'>" +
+      "shopping_bag_speed" +
+      "</span>";
+
+    a.innerHTML = icono + " Salir";
+    a.addEventListener("click", function () {
+      closeSession();
+    });
+    li.appendChild(a);
+
+    isExistMenu.appendChild(li);
+  }
+}
+
+function closeSession() {
+  localStorage.removeItem("sesion");
+  window.location.reload();
+}
 
 function signIn(event) {
   event.preventDefault(); // evita el envio automatico
 
-  const email = document.getElementById("email").value.trim();
-  const password = document.getElementById("password").value.trim();
+  const email = document.getElementById("email");
+  const password = document.getElementById("password");
+  const message = document.getElementById("message");
 
-  const emailInput = document.getElementById("email");
+  // limpiar las clases en el input
+  email.classList.remove("error");
+  email.classList.remove("success");
+  password.classList.remove("error");
+  password.classList.remove("success");
 
-  // limpia las clases en el input
-  emailInput.classList.remove("error");
-  emailInput.classList.remove("success");
-
-  if (email === "") {
-    emailInput.classList.add("error");
+  if (email.value.trim() === "") {
+    email.classList.add("error");
   }
 
-  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  if (!emailRegex.test(email)) {
-    emailInput.classList.add("error");
+  console.log("valores:", email.value, password.value);
+  console.log(!expresionEmail.test(email.value.trim()));
+  if (!expresionEmail.test(email.value.trim())) {
+    email.classList.add("error");
+    message.textContent = "Correo no válido";
+    message.classList.add("message-error");
     return;
   }
-  emailInput.classList.add("success");
 
-  console.log(email, password);
-  // window.location.href = "https://www.google.com";
+  if (password.value.trim() === "") {
+    password.classList.add("error");
+  }
+
+  if (!expresionPassword.test(password.value.trim())) {
+    password.classList.add("error");
+    message.textContent = "Contraseña no válida";
+    return;
+  }
+
+  email.classList.add("success");
+  password.classList.add("success");
+  message.classList.add("message-success");
+  message.textContent = "Iniciando...";
+
+  const sessionActive = "1";
+
+  localStorage.setItem("sesion", sessionActive);
+
+  console.log("fin del script");
+  window.location.href = "index.html";
 }
-
-function validEmail() {}
